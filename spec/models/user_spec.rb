80e3d9cea8dcd5f388854a0c20e94cd9dd289fd9
Pavel Perkin <perkinpv@gmail.com>
2017-12-05 05:37:31 +0200

    Validate email uniq

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe User, type: :model do
   subject { build(:user) }
 
   it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to validate_confirmation_of(:password) }
   it { is_expected.to allow_value('admin@lohika.com').for(:email) }
